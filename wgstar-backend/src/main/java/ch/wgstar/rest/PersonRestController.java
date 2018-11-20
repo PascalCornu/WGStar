059ccf154b39e46920cb7e30243046b4e49f833b
ue82371 <florian.borter@sbb.ch>
2018-11-20 13:23:20 +0100

    Person can be saved

--- wgstar-backend/src/main/java/ch/wgstar/rest/PersonRestController.java
+++ wgstar-backend/src/main/java/ch/wgstar/rest/PersonRestController.java
@@ -8,11 +8,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -28,5 +30,19 @@ public class PersonRestController {
         Set<PersonView> collection = list.stream().map(PersonView::from).collect(Collectors.toSet());
         String result = JsonUtil.writeValueAsString(collection);
         return new ResponseEntity<>(result, HttpUtils.headers(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/person/save", method = RequestMethod.POST)
+    public void saveWg(@RequestBody Map<String, Object> payload) {
+        /*WG wg = wgRepository.getOne((Long) payload.get("id"));
+        if(wg == null){
+            wg = new WG();
+        }*/
+        Person person = new Person();
+        person.setFirstname((String) payload.get("firstname"));
+        person.setLastname((String) payload.get("lastname"));
+        person.setEmail((String) payload.get("email"));
+        person.setPassword((String) payload.get("password"));
+        personRepository.save(person);
     }
 }
